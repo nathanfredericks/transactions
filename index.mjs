0205f90ca9e4e6a2b3df8f83554b58f0f6a70ba5
Nathan Fredericks <nathan@fredericks.dev>
2024-08-05 13:09:22 -0300

    Improve reliability and change how transactions are imported

--- index.mjs
+++ index.mjs
@@ -10,16 +10,45 @@ const ynabAPI = new ynab.API(process.env.YNAB_ACCESS_TOKEN);
 const bucketName = "nathanfredericks-transactions";
 const budgetId = "e0e7f122-6f2f-41f3-9b84-6d8f49fd5eab";
 
+const amountRegex = /\$(?<amount>[0-9]+\.[0-9]{2})/;
+
 const tangerineCreditCard = {
   ynabAccountId: "b8df1b5f-4163-43ce-9bd0-01cb1f824cf7",
   emailSubject: "A new Credit Card transaction has been made",
-  emailRegex: /\$(?<amount>[0-9]+\.[0-9]{2}).*at (?<payee>.*) on/s,
+  emailPayeeRegex: /\sat\s(?<payee>.*)\son\s/s,
 };
 
 const bmoCreditCard = {
   ynabAccountId: "7920fe4c-4deb-4b33-a860-3b2b0d80085f",
   emailSubject: "BMO Credit Card Alert",
-  emailRegex: /\$(?<amount>[0-9]{2}\.[0-9]{2}).*at\W(?<payee>.*) was approved/s,
+  emailPayeeRegex: /\sat\s(?<payee>.*)\swas\s/s,
+};
+
+const newTransaction = async ({
+  accountId,
+  date,
+  amount,
+  payee,
+  imported = false,
+}) => {
+  return await ynabAPI.transactions.createTransaction(budgetId, {
+    transaction: {
+      account_id: accountId,
+      date: date
+        .toLocaleString("en-CA", { timeZone: "America/Halifax" })
+        .split(",")[0],
+      amount: parseFloat(amount) * -1000,
+      payee_id: null,
+      payee_name: payee,
+      category_id: null,
+      memo: null,
+      cleared: "uncleared",
+      approved: false,
+      flag_color: null,
+      subtransactions: [],
+      import_id: imported ? uuidv4() : null,
+    },
+  });
 };
 
 export const handler = async (event) => {
@@ -35,54 +64,68 @@ export const handler = async (event) => {
   // Tangerine
   if (message.subject === tangerineCreditCard.emailSubject) {
     const text = convert(message.html);
-    const { amount, payee } = tangerineCreditCard.emailRegex.exec(text).groups;
+    const { amount } = amountRegex.exec(text).groups;
+    const { payee } = tangerineCreditCard.emailPayeeRegex.exec(text).groups;
     try {
-      await ynabAPI.transactions.createTransaction(budgetId, {
-        transaction: {
-          account_id: tangerineCreditCard.ynabAccountId,
-          date: message.date.toLocaleString("en-CA").split(",")[0],
-          amount: parseFloat(amount) * -1000,
-          payee_id: null,
-          payee_name: payee,
-          category_id: null,
-          memo: null,
-          cleared: "uncleared",
-          approved: false,
-          flag_color: null,
-          subtransactions: [],
-          import_id: uuidv4(),
-        },
+      const transaction = {
+        accountId: tangerineCreditCard.ynabAccountId,
+        date: message.date,
+        amount: amount,
+        payee: payee,
+      };
+      // Create new imported transaction
+      const importedTransaction = await newTransaction({
+        ...transaction,
+        imported: true,
       });
-      return true;
+      // Delete imported transaction
+      await ynabAPI.transactions.deleteTransaction(
+        budgetId,
+        importedTransaction.data.transaction.id,
+      );
+      // Create new user-entered transaction with imported transaction payee
+      // User-entered transactions do not follow payee naming rules
+      await newTransaction({
+        ...transaction,
+        payee: importedTransaction.data.transaction.payee_name,
+      });
+      return null;
     } catch (e) {
-      console.error("Error creating YNAB transaction:", e);
+      throw new Error("Error importing transaction to YNAB", e);
     }
     // BMO
   } else if (message.subject === bmoCreditCard.emailSubject) {
     const text = convert(message.html);
-    const { amount, payee } = bmoCreditCard.emailRegex.exec(text).groups;
+    const { amount } = amountRegex.exec(text).groups;
+    const { payee } = bmoCreditCard.emailPayeeRegex.exec(text).groups;
     try {
-      await ynabAPI.transactions.createTransaction(budgetId, {
-        transaction: {
-          account_id: bmoCreditCard.ynabAccountId,
-          date: message.date.toLocaleString("en-CA").split(",")[0],
-          amount: parseFloat(amount) * -1000,
-          payee_id: null,
-          payee_name: payee,
-          category_id: null,
-          memo: null,
-          cleared: "uncleared",
-          approved: false,
-          flag_color: null,
-          subtransactions: [],
-          import_id: uuidv4(),
-        },
+      const transaction = {
+        accountId: bmoCreditCard.ynabAccountId,
+        date: message.date,
+        amount: amount,
+        payee: payee,
+      };
+      // Create new imported transaction
+      const importedTransaction = await newTransaction({
+        ...transaction,
+        imported: true,
       });
-      return true;
+      // Delete imported transaction
+      await ynabAPI.transactions.deleteTransaction(
+        budgetId,
+        importedTransaction.data.transaction.id,
+      );
+      // Create new user-entered transaction with imported transaction payee
+      // User-entered transactions do not follow payee naming rules
+      await newTransaction({
+        ...transaction,
+        payee: importedTransaction.data.transaction.payee_name,
+      });
+      return null;
     } catch (e) {
-      console.error("Error creating YNAB transaction:", e);
+      throw new Error("Error importing transaction to YNAB", e);
     }
   }
 
-  return false;
+  throw new Error("Transaction notification not supported");
 };
