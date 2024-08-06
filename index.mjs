23c9b7c79ccc5b20179d4c979e97ba44af6cb2ef
Nathan Fredericks <nathan@fredericks.dev>
2024-08-06 20:32:16 -0300

    Switch to Gemini AI and use AI for parsing email
    Regex is not reliable since the body can change at any time

--- index.mjs
+++ index.mjs
@@ -2,228 +2,103 @@ import AWS from "aws-sdk";
 import * as ynab from "ynab";
 import { simpleParser } from "mailparser";
 import { convert } from "html-to-text";
-import OpenAI from "openai";
-import Airtable from "airtable";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const s3 = new AWS.S3();
 const secretsManager = new AWS.SecretsManager();
 const { SecretString } = await secretsManager
   .getSecretValue({
-    SecretId: process.env.SECRET,
+    SecretId: process.env.SECRET_NAME,
   })
   .promise();
-const { OPENAI_API_KEY, YNAB_ACCESS_TOKEN, AIRTABLE_API_KEY } =
-  JSON.parse(SecretString);
+const { YNAB_ACCESS_TOKEN, GEMINI_API_KEY } = JSON.parse(SecretString);
 const ynabAPI = new ynab.API(YNAB_ACCESS_TOKEN);
-const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
-});
-
-Airtable.configure({ apiKey: AIRTABLE_API_KEY });
-const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
-
-const amountRegex = /\$(?<amount>[0-9]+\.[0-9]{2})/;
 
 const tangerineCreditCard = {
   ynabAccountId: process.env.YNAB_TANGERINE_ACCOUNT_ID,
   emailAddress: "donotreply@tangerine.ca",
   emailSubject: "A new Credit Card transaction has been made",
-  emailPayeeRegex: /of.*\sat\s(?<payee>.*)\son\s/s,
 };
 
 const bmoCreditCard = {
   ynabAccountId: process.env.YNAB_BMO_ACCOUNT_ID,
   emailAddress: "bmoalerts@bmo.com",
   emailSubject: "BMO Credit Card Alert",
-  emailPayeeRegex: /of.*\sat\s(?<payee>.*)\swas\s/s,
-};
-
-const getOverrides = async () => {
-  const payeesTable = base("Payees");
-  const overridesTable = base("Overrides");
-
-  console.info("Selecting all payees from Airtable");
-  const payeesRecords = await payeesTable
-    .select({
-      view: "Grid view",
-    })
-    .all();
-  console.debug(
-    "Selected all payees from Airtable",
-    JSON.stringify(payeesRecords, null, 2),
-  );
-
-  console.info("Searching for payees with overrides");
-  const payeesRecordsWithOverrides = payeesRecords.filter(
-    (record) => !!record.get("Overrides"),
-  );
-  console.debug(
-    "Found payees with overrides",
-    JSON.stringify(payeesRecordsWithOverrides, null, 2),
-  );
-
-  console.info("Fetching payee overrides from Airtable");
-  const results = await Promise.all(
-    payeesRecordsWithOverrides.map(async (payee) => {
-      const payeeName = payee.get("Name");
-      const overrideIds = payee.get("Overrides");
-
-      const overrides = await Promise.all(
-        overrideIds.map(async (overrideId) => {
-          const override = await overridesTable.find(overrideId);
-          return override.get("Credit Card Transaction");
-        }),
-      );
-
-      return `${overrides.join(" or ")} = ${payeeName}`;
-    }),
-  );
-  console.debug(
-    "Fetched payee overrides from Airtable",
-    JSON.stringify(results, null, 2),
-  );
-  return results.join("\n");
-};
-
-const newTransaction = async ({ accountId, date, amount, payee }) => {
-  console.info("Fetching payees from YNAB");
-  const payees = (
-    await ynabAPI.payees.getPayees(process.env.YNAB_BUDGET_ID)
-  ).data.payees
-    .filter((payee) => !payee.transfer_account_id && !payee.deleted)
-    .map((payee) => payee.name)
-    .join("\n");
-  console.debug("Fetched payees from YNAB", JSON.stringify(payees, null, 2));
-  console.info("Fetching overrides from Airtable");
-  const overrides = await getOverrides();
-  console.info("Sending payee matching request to OpenAI");
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini-2024-07-18",
-    messages: [
-      {
-        role: "system",
-        content: [
-          {
-            type: "text",
-            text: `Match the credit card transaction to a payee from the list provided. If no match is found, create a new payee using the merchant or company name, ensuring it's less than 200 characters. Output only the payee name.
-PayPal, Paddle, FS and SQ are payment processesors, not merchants.
-Overrides:
-${overrides}
-Payees:
-${payees}`,
-          },
-        ],
-      },
-      {
-        role: "user",
-        content: [
-          {
-            type: "text",
-            text: payee,
-          },
-        ],
-      },
-    ],
-    temperature: 1,
-    max_tokens: 75,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  console.debug("Matched payee with OpenAI", response.choices[0].message.content)
-
-  console.info("Creating new user-entered transaction in YNAB");
-  return await ynabAPI.transactions.createTransaction(
-    process.env.YNAB_BUDGET_ID,
-    {
-      transaction: {
-        account_id: accountId,
-        date: date
-          .toLocaleString("en-CA", { timeZone: process.env.TZ })
-          .split(",")[0],
-        amount: Math.ceil(parseFloat(amount) * -1000),
-        payee_id: null,
-        payee_name: response.choices[0].message.content,
-        category_id: null,
-        memo: null,
-        cleared: "uncleared",
-        approved: false,
-        flag_color: null,
-        subtransactions: [],
-        import_id: null,
-      },
-    },
-  );
 };
 
 export const handler = async (event) => {
   const notification = JSON.parse(event.Records[0].Sns.Message);
   const object = await s3
     .getObject({
-      Bucket: process.env.BUCKET_NAME,
+      Bucket: process.env.S3_BUCKET_NAME,
       Key: notification.mail.messageId,
     })
     .promise();
-  console.info("Parsing email message");
   const message = await simpleParser(object.Body);
-  console.debug("Parsed email message", JSON.stringify(message, null, 2));
-  // Tangerine
-  if (
+  const text = convert(message.html);
+
+  const isTangerineNotification =
     message.from.value[0].address === tangerineCreditCard.emailAddress &&
-    message.subject === tangerineCreditCard.emailSubject
-  ) {
-    console.info("Converting Tangerine email message body to plain text");
-    const text = convert(message.html);
-    console.debug(
-      "Converted Tangerine email message body to plain text",
-      JSON.stringify(text, null, 2),
-    );
-    console.info("Searching message body for amount and payee");
-    const { amount } = amountRegex.exec(text).groups;
-    const { payee } = tangerineCreditCard.emailPayeeRegex.exec(text).groups;
-    console.debug(
-      "Found amount and payee in message body",
-      JSON.stringify({ amount, payee }, null, 2),
-    );
-    try {
-      await newTransaction({
-        accountId: tangerineCreditCard.ynabAccountId,
-        date: message.date,
-        amount: amount,
-        payee: payee,
-      });
-    } catch (e) {
-      console.error("Error importing transaction to YNAB", e);
-    }
-    // BMO
-  } else if (
+    message.subject === tangerineCreditCard.emailSubject;
+  const isBmoNotification =
     message.from.value[0].address === bmoCreditCard.emailAddress &&
-    message.subject === bmoCreditCard.emailSubject
-  ) {
-    console.info("Converting BMO email message body to plain text");
-    const text = convert(message.html);
-    console.debug(
-      "Converted BMO email message body to plain text",
-      JSON.stringify(text, null, 2),
-    );
-    console.info("Searching message body for amount and payee");
-    const { amount } = amountRegex.exec(text).groups;
-    const { payee } = bmoCreditCard.emailPayeeRegex.exec(text).groups;
-    console.debug(
-      "Found amount and payee in message body",
-      JSON.stringify({ amount, payee }, null, 2),
-    );
-    try {
-      await newTransaction({
-        accountId: bmoCreditCard.ynabAccountId,
-        date: message.date,
-        amount: amount,
-        payee: payee,
-      });
-    } catch (e) {
-      console.error("Error importing transaction to YNAB", e);
-    }
-  } else {
-    console.error("Transaction notification not supported");
+    message.subject === bmoCreditCard.emailSubject;
+
+  if (!isTangerineNotification && !isBmoNotification) {
+    return console.error("Bank not supported");
   }
+
+  const accountId = isTangerineNotification
+    ? tangerineCreditCard.ynabAccountId
+    : bmoCreditCard.ynabAccountId;
+
+  const payees = (
+    await ynabAPI.payees.getPayees(process.env.YNAB_BUDGET_ID)
+  ).data.payees
+    .filter((payee) => !payee.transfer_account_id && !payee.deleted)
+    .map((payee) => payee.name)
+    .join("\n");
+
+  const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: `You will be provided with a credit card alert, and your task is to extract the amount and merchant from it. Once you have extracted the merchant, match it to a payee from the provided list. If no match is found, create a new human-readable payee using the merchant name, ensuring it's less than 200 characters. PayPal, Paddle (PADDLE.NET), FastSpring (FS), and Square (SQ) are payment processors, not merchants. Write your output in JSON format with "amount" and "payee" keys.
+    Payee overrides:
+    SOBEY'S = Sobeys
+    HEATHER'S YIG or WOLFVILLE SAVE EASY = Independent
+    WF = Wayfair
+    CIRCLE K / IRVING = Irving Oil
+    NEW MINAS SUPERS or KINGSTON SUPERSTORE = Real Canadian Superstore
+    SOBEYS FAST FUEL = Fast Fuel
+    NEEDS CAR WASH = Fast Fuel
+    Payees:
+    ${payees}`,
+  });
+
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "application/json",
+  };
+
+  const chatSession = model.startChat({
+    generationConfig,
+  });
+
+  const { response } = await chatSession.sendMessage(text);
+  const { amount, payee } = JSON.parse(response.text());
+
+  await ynabAPI.transactions.createTransaction(process.env.YNAB_BUDGET_ID, {
+    transaction: {
+      account_id: accountId,
+      date: message.date
+        .toLocaleString("en-CA", { timeZone: "America/Halifax" })
+        .split(",")[0],
+      amount: Math.ceil(parseFloat(amount) * -1000),
+      payee_name: payee,
+      cleared: "uncleared",
+    },
+  });
 };
