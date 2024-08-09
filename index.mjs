ff407d530c359afe3c060fa0e12b9633e3df441e
Nathan Fredericks <nathan@fredericks.dev>
2024-08-09 11:48:50 -0300

    Update to AWS SDK v3, switch back to OpenAI, add back dynamic overrides

--- index.mjs
+++ index.mjs
@@ -1,18 +1,29 @@
-import AWS from "aws-sdk";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import * as ynab from "ynab";
 import { simpleParser } from "mailparser";
 import { convert } from "html-to-text";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import OpenAI from "openai";
+import { z } from "zod";
+import { zodResponseFormat } from "openai/helpers/zod";
 
-const s3 = new AWS.S3();
-const secretsManager = new AWS.SecretsManager();
-const { SecretString } = await secretsManager
-  .getSecretValue({
+const s3Client = new S3Client();
+const dynamoDBClient = new DynamoDBClient();
+const secretsManagerClient = new SecretsManagerClient();
+const { SecretString } = await secretsManagerClient.send(
+  new GetSecretValueCommand({
     SecretId: process.env.SECRET_NAME,
-  })
-  .promise();
-const { YNAB_ACCESS_TOKEN, GEMINI_API_KEY } = JSON.parse(SecretString);
+  }),
+);
+const { YNAB_ACCESS_TOKEN, OPENAI_API_KEY } = JSON.parse(SecretString);
 const ynabAPI = new ynab.API(YNAB_ACCESS_TOKEN);
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
 
 const tangerineCreditCard = {
   ynabAccountId: process.env.YNAB_TANGERINE_ACCOUNT_ID,
@@ -29,15 +40,22 @@ const bmoCreditCard = {
 export const handler = async (event) => {
   try {
     const notification = JSON.parse(event.Records[0].Sns.Message);
-    const object = await s3
-      .getObject({
+    const { Body } = await s3Client.send(
+      new GetObjectCommand({
         Bucket: process.env.S3_BUCKET_NAME,
         Key: notification.mail.messageId,
-      })
-      .promise();
-    const message = await simpleParser(object.Body);
-    const text = convert(message.html);
-
+      }),
+    );
+    const message = await simpleParser(Body);
+    const text = convert(message.html, {
+      selectors: [
+        { selector: "a", options: { ignoreHref: true } },
+        {
+          selector: "img",
+          format: "skip",
+        },
+      ],
+    });
     const isTangerineNotification =
       message.from.value[0].address === tangerineCreditCard.emailAddress &&
       message.subject === tangerineCreditCard.emailSubject;
@@ -53,43 +71,51 @@ export const handler = async (event) => {
       ? tangerineCreditCard.ynabAccountId
       : bmoCreditCard.ynabAccountId;
 
+    const { Items } = await dynamoDBClient.send(
+      new ScanCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+      }),
+    );
+    const formatOverrides = (overrides) => {
+      return overrides.reduce((accumulator, currentValue) => {
+        const merchant = currentValue.merchant.S;
+        const payee = currentValue.payee.S;
+        accumulator[merchant] = payee;
+        return accumulator;
+      }, {});
+    };
+    const overrides = formatOverrides(Items);
+
     const payees = (
       await ynabAPI.payees.getPayees(process.env.YNAB_BUDGET_ID)
     ).data.payees
       .filter((payee) => !payee.transfer_account_id && !payee.deleted)
-      .map((payee) => payee.name)
-      .join("\n");
+      .map((payee) => payee.name);
 
-    const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro",
-      systemInstruction: `You will be provided with a credit card alert, and your task is to extract the amount and merchant from it. Once you have extracted the merchant, match it to a payee from the provided list. If no match is found, create a new human-readable payee using the merchant name, ensuring it's less than 200 characters. PayPal, Paddle (PADDLE.NET), FastSpring (FS), and Square (SQ) are payment processors, not merchants. Write your output in JSON format with "amount" and "payee" keys.
-    Payee overrides:
-    SOBEY'S = Sobeys
-    HEATHER'S YIG or WOLFVILLE SAVE EASY = Independent
-    WF = Wayfair
-    CIRCLE K / IRVING = Irving Oil
-    NEW MINAS SUPERS or KINGSTON SUPERSTORE = Real Canadian Superstore
-    SOBEYS FAST FUEL = Fast Fuel
-    NEEDS CAR WASH = Fast Fuel
-    Payees:
-    ${payees}`,
+    const TransactionExtraction = z.object({
+      amount: z.number(),
+      payee: z.string(),
     });
 
-    const generationConfig = {
-      temperature: 1,
-      topP: 0.95,
-      topK: 64,
-      maxOutputTokens: 8192,
-      responseMimeType: "application/json",
-    };
-
-    const chatSession = model.startChat({
-      generationConfig,
+    const completion = await openai.beta.chat.completions.parse({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: `You will be provided with a credit card alert, and your task is to extract the amount and merchant from it. Once you have extracted the merchant, match it to a payee from the provided list. If no match is found, create a new human-readable payee using the merchant name, ensuring it's less than 200 characters. PayPal, Paddle (PADDLE.NET), FastSpring (FS), and Square (SQ) are payment processors, not merchants. You should convert the amount and payee to the given structure.
+Payee overrides:
+${JSON.stringify(overrides)}
+Payees:
+${JSON.stringify(payees)}`,
+        },
+        { role: "user", content: text },
+      ],
+      response_format: zodResponseFormat(
+        TransactionExtraction,
+        "transaction_extraction",
+      ),
     });
-
-    const { response } = await chatSession.sendMessage(text);
-    const { amount, payee } = JSON.parse(response.text());
+    const { amount, payee } = completion.choices[0].message.parsed;
 
     await ynabAPI.transactions.createTransaction(process.env.YNAB_BUDGET_ID, {
       transaction: {
@@ -97,7 +123,7 @@ export const handler = async (event) => {
         date: message.date
           .toLocaleString("en-CA", { timeZone: "America/Halifax" })
           .split(",")[0],
-        amount: Math.round(parseFloat(amount) * -1000),
+        amount: Math.round(amount * -1000),
         payee_name: payee,
         cleared: "uncleared",
       },
